de8816147f5583d6e422083f4e2189e41757ca30
Naveen RS <rnaveen.tkt@gmail.com>
2023-10-02 12:52:58 +0530

    project folder structure added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # smartphone_customer_review_sentiment_analyzer_app
-Analyses the smartphone customer review with NLP techniques and neural networks models and shows overall polarity of the review based on features of phones
+A Python App which Analyses the smartphone customer review with NLP techniques and neural networks models and shows overall polarity of the review based on features of phones
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "smartphone_customer_review_sentiment_analyzer_app"
+AUTHOR_USER_NAME = "naveentnj"
+SRC_REPO = "textSentimentAnalyser"
+AUTHOR_EMAIL = "rnaveen.tkt@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python App which Analyses the smartphone customer review with NLP techniques and neural networks models and shows overall polarity of the review based on features of phones",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
